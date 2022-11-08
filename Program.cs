11c17be41395db746f7b95b39c6c5a525b5af63b
PALOMA AZEVEDO SANTOS <paloma.asantos17@senacsp.edu.br>
2022-11-07 22:14:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Agenda_Lib.DAO.CEP;
+using System;
+
+namespace Agenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ViaCEP oViaCEP = new ViaCEP();
+            DadosCEP oDadosCEP = new DadosCEP();
+            oViaCEP = oDadosCEP.Pesquisar_CEP("06142050");
+        }
+    }
+}
